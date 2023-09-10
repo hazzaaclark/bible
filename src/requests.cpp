61a96f9b4353eadb35c7118fda099f352f57b31f
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-09-10 21:33:24 +0100

    Update requests.cpp

--- src/requests.cpp
+++ src/requests.cpp
@@ -17,6 +17,25 @@ static char* INIT_REQUEST(char* HOSTNAME, char* REQUEST_DOMAIN, char* REQUEST)
 {
 	REQUEST += 0, sizeof(REQUEST);
 	HTTP += 1, sizeof(HTTP_BUFFER_SIZE);
+
+	free(HTTP);
+	return REQUEST;
+}
+
+static U32* MAKE_REQUEST(U32 SOCKET, char* HOSTNAME, char* REQUEST, char* REQUEST_PATH)
+{
+	UNK BYTES_SENT = 0;
+	UNK TOTAL_BYTES_SENT = 0;
+	UNK BYTES_LENGTH = strlen(REQUEST);
+
+	REQUEST += 1, sizeof(INIT_REQUEST(HOSTNAME, REQUEST_PATH, REQUEST));
+
+	while (1)
+	{
+		BYTES_SENT += send(SOCKET, REQUEST, strlen(REQUEST), NULL);
+		TOTAL_BYTES_SENT += sizeof(BYTES_SENT);
+	}
+
 }
 
 #endif
