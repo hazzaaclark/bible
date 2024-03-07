a58f0d99e6ab0588e43701496ec18c7ca2026a52
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-03-07 19:59:52 +0000

    Update Constants.java

--- src/Constants.java
+++ src/Constants.java
@@ -12,4 +12,14 @@ public interface Constants
     public final Scanner SCANNER = new Scanner(System.in);
     public final static StringBuilder MENU = new StringBuilder();
     public static boolean PRINT_ARG = false;
+
+    /* THE FOLLOWING FUNCTIONS PERTAIN TOWARDS */
+    /* GENERAL PURPOSE METHODS FOR ACCESSING INFORMATION */
+    /* PERTAINING TOWARDS BOOKS, CHAPTER AND VERSES ALIKE */
+
+    private static String GET_WORD_INFO(Scanner SCANNER, String QUESTION)
+    {
+        System.out.println(QUESTION + ":");
+        return SCANNER.next();
+    }
 }
