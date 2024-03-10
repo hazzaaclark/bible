a3dc300ccdae148ca51483038e8092f8bae7cef3
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-03-10 23:16:33 +0000

    updated methods for parser

--- src/Constants.java
+++ src/Constants.java
@@ -11,7 +11,14 @@ public interface Constants
 {
     public final Scanner SCANNER = new Scanner(System.in);
     public final static StringBuilder MENU = new StringBuilder();
+    public final static StringBuilder BIBLE_PARSER = new StringBuilder();
     public static boolean PRINT_ARG = false;
+
+    /* ARBRITARY STRING LITERAL FOR BEING ABLE  */
+    /* TO PARSE LENGTHS OF A VERSE, BOOK AND OR CHAPTER */
+
+    public static String BIBLE_START_OFFSET = "";
+    public static String BIBLE_END_OFFSET = ""; 
 
     /* THE FOLLOWING FUNCTIONS PERTAIN TOWARDS */
     /* GENERAL PURPOSE METHODS FOR ACCESSING INFORMATION */
@@ -21,5 +28,21 @@ public interface Constants
     {
         System.out.println(QUESTION + ":");
         return SCANNER.next();
+    }
+
+    private static int GET_NUMBER_INPUT(Scanner SCANNER, String QUESTION)
+    {
+        System.out.println(QUESTION + ":");
+        return SCANNER.nextInt();
+    }
+
+    private static String GET_VERSE_INFO(Scanner SCANNER)
+    {
+        return GET_WORD_INFO(SCANNER, "Enter the range of the verses you want to search for!\n");
+    }
+
+    private static int GET_CHAPTER_INFO(Scanner SCANNER)
+    {
+        return GET_NUMBER_INPUT(SCANNER, "Enter the Chapter Number\n");
     }
 }
