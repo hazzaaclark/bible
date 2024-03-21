dac806e2fd0b043909e4cf54e4f9a249a51e9c4c
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-03-21 22:32:41 +0000

    dictionary extends Bible

--- src/api/Dictionary.java
+++ src/api/Dictionary.java
@@ -15,6 +15,7 @@ package api;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /* JSOUP INCLUDES */
@@ -24,15 +25,26 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public final class Dictionary 
+public class Dictionary extends Bible
 {
     private static final int BOOKS_OT = 39;
     private static final int BOOKS_NT = 27;
     private static final int BOOKS_ALL = 66;
     private static List<Book> BIBLE_BOOKS;
+    protected static String FILE_NAME = "KJV.xml";
 
-    public Dictionary(String BOOK)
+    public Dictionary()
     {
+        super(BIBLE_BOOKS);
+    }
 
+    /* ALL OF THE PRECEEDING ARE GENERAL PURPOSE METHODS FOR */
+    /* PARSING INFORMATION PERTAINING TOWARDS THE CONTENTS OF THE FILE */
+
+    /* READ THROUGH THE HEADER TO DETERMINE THE LENGTH BASED ON THE USER INPUT */
+
+    public Bible INDEX_BOOKS()
+    {
+        return new Bible(BIBLE_BOOKS);
     }
 }
