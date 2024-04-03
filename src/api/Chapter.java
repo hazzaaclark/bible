c6e51d15e5e8a596d2997d359975d3244ca26a82
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-04-03 22:04:49 +0100

    Update Chapter.java

--- src/api/Chapter.java
+++ src/api/Chapter.java
@@ -12,7 +12,48 @@ package api;
 
 import java.util.List;
 
-public class Chapter 
+public class Chapter implements Constants
 {
-    
+    private int CHAPTER_NUMBER;
+    private String CHAPTER_DESCRIPTION; 
+    private List<Verse> VERSES;
+
+    public Chapter(int CHAPTER_NO)
+    {
+        this.CHAPTER_NUMBER = CHAPTER_NO;
+    }
+
+    public List<Verse> GET_VERSE()
+    {
+        return VERSES;
+    }
+
+    public int GET_CHAPTER_NUMBER()
+    {
+        return CHAPTER_NUMBER;
+    }
+
+    public void SET_CHAPTER_DESC(String DESCRIPTION)
+    {
+        this.CHAPTER_DESCRIPTION = DESCRIPTION;
+    }
+
+    public String GET_CHAPTER_DESC()
+    {
+        return CHAPTER_DESCRIPTION;
+    }
+
+    @Override
+    public String toString()
+    {
+        BIBLE_PARSER.append("Chapter: ").append(GET_CHAPTER_NUMBER()).append(": ").append(Verse.GET_TEXT()).append("\n");
+
+        for(Verse V : VERSES)
+        {
+            BIBLE_PARSER.append("Verse: ").append(Verse.GET_VERSE_NUMBER()).append(": ").append(Verse.GET_TEXT()).append("\n");
+        }
+
+        return BIBLE_PARSER.toString();
+    }
 }
+
