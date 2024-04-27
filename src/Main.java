6a2af5f02a3a498ad00eab77d072d6e73d9570df
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-04-27 17:15:48 +0100

    updated second choice

--- src/Main.java
+++ src/Main.java
@@ -13,22 +13,17 @@ import api.Constants;
 import api.Dictionary;
 import api.Verse;
 
-public class Main extends Bible implements Constants 
+public class Main
 {
     private static int INDEX = 0;
     private static Scanner CHOICE_SCAN = new Scanner(System.in);
     private static String CHAPTER_NUMBER = new String();
     private static String URL = "https://www.bible.com/verse-of-the-day";
     private static String VERSE;
-
-    public Main(List<Book> BOOKS) 
-    {
-        super(BOOKS);
-    }
-
     public static void main(String[] args)
     {
         DISPLAY_MENU();
+        CHOICE_SCAN.close();
     }
 
     public static void DISPLAY_MENU()
@@ -37,9 +32,8 @@ public class Main extends Bible implements Constants
         Constants.MENU.append("Harry Clark - Bible Parser \n");
         Constants.MENU.append("                           \n");
         Constants.MENU.append("Options:                   \n");    
-        Constants.MENU.append("1. Search Chapter          \n");   
-        Constants.MENU.append("2. Search Verses           \n");
-        Constants.MENU.append("3. Verse Of The Day        \n");
+        Constants.MENU.append("1. Search Verses           \n");   
+        Constants.MENU.append("2. Verse Of The Day        \n");
         Constants.MENU.append("===========================\n");
 
         System.out.println(Constants.MENU.toString());
@@ -49,6 +43,10 @@ public class Main extends Bible implements Constants
 
     private static void USER_CHOICE() 
     {
+        List<Book> BOOKS = null;
+        Bible BIBLE = new Bible(BOOKS);
+        Verse VERSE_BASE = new Verse(INDEX, VERSE);
+        
         try 
         {
             INDEX = Constants.SCANNER.nextInt();
@@ -57,9 +55,19 @@ public class Main extends Bible implements Constants
             switch (INDEX) 
             {
                 case 1:
-                    System.out.print("Enter the chapter number: ");
-                    CHAPTER_NUMBER = Constants.SCANNER.nextLine();
-                    Bible.PRINT_SEARCH(Constants.GET_WORD_INFO(CHOICE_SCAN));
+                    System.out.println("Enter the Book Name (e.g, Rom, Phil, Tim): ");
+                    String BOOK_NAME = Constants.SCANNER.nextLine();
+                    System.out.println("Enter the Chapter Number: ");
+                    int CHAPTER_NO = Constants.SCANNER.nextInt();
+                    System.out.println("Enter the Verse Number: ");
+                    int VERSE_NO = Constants.SCANNER.nextInt();
+
+                    BIBLE.SEARCH_VERSE(BOOK_NAME, CHAPTER_NO, VERSE_NO);
+                    break;
+
+                case 2:
+                    VERSE = VERSE_BASE.GET_VOTD();
+                    System.out.println(VERSE);
                     break;
                 
                 default:
